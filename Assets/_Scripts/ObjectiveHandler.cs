a99c7c39f079aaef97200f693fc4a21f22374e76
Sebastian <sehor5915@uib.no>
2024-03-19 23:46:28 +0100

    vr mechanics

---
+++ Assets/_Scripts/ObjectiveHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var objective = GameObject.Find("Goal");
+        var lookPos = objective.transform.position - transform.position;
+        var rotation = Quaternion.LookRotation(lookPos);
+        rotation *= Quaternion.Euler(-90, 0, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 1); 
+    }
+}
